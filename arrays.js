b00c1089ec78bd1c3a83d5028ec34372354467c0
unknown <daveasmah@yahoo.com>
2022-07-06 23:16:36 -0400

    second commit;

---
+++ arrays.js
@@ -0,0 +1,11 @@
+//Write a JavaScript function that takes in two parameters, an array and a number (n), 
+//and return the first n elements of that array.
+
+// Write an array of names
+
+// write a function with 2 parameters ie. an array and a number(n)
+
+// write conditional statement to validate input and return corresponding output
+
+// use the .slice method to return the required items of the original array
+
